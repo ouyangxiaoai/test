efeca4de0eb13f5bdae4c47273a4e93cc3e3ce29
yangxiu <xiu_yang@kingdee.com>
2021-12-20 15:12:07 +0800

    feat(项目配置): 更新了项目配置
    1. 更新git版本防止显示乱码
    2. 添加了符合angular的提交模板
    3. 添加了commitlint检测提交是否符合，并且通过husky进行触发
    4. 通过conventional-changelog-cli自动生成changelog

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 	<div>
 		<img alt="Vue logo" src="./assets/logo.png" />
 		<HelloWorld msg="Welcome to Your Vue.js App" />
+		<div></div>
 	</div>
 </template>
 
@@ -18,7 +19,7 @@ export default {
 
 <style>
 #app {
-	font-family: Avenir, Helvetica, Arial, sans-serif;
+	font-family: Helvetica, Arial, sans-serif;
 	-webkit-font-smoothing: antialiased;
 	-moz-osx-font-smoothing: grayscale;
 	text-align: center;
